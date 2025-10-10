0e3bd8cc191af52589f6a6f9519fc20139ffa535
lucas soares <lucassoaresti@yahoo.com>
2025-10-09 21:34:52 -0300

    feat: adicionado, paginação, filtragem por categoria e filtragem por nome

--- src/components/CardapioContent.jsx
+++ src/components/CardapioContent.jsx
@@ -1,29 +1,22 @@
-import { ArrowDownNarrowWide } from "lucide-react";
+import { ArrowDownNarrowWide, Target } from "lucide-react";
 import { useContext } from "react";
 import { ContextApp } from "../context/Context-app";
 
 function CardapioContent() {
-  const { isMenuCardapioOpen, setisMenuCardapioOpen } = useContext(ContextApp);
-  function tempdiv() {
-    return (
-      <div
-        className={` rounded-md shadow-2xl shadow-black  bg-center bg-cover grid grid-rows-2 h-[190px] w-[210px] hover:scale-110`}
-        style={{
-          backgroundImage: `url("https://www.sabornamesa.com.br/media/k2/items/cache/58d52ea025b256e4ce8690fddc53cbd4_XL.jpg")`,
-        }}
-      >
-        <div></div>
-        <div className="bg-gradient-to-t from-[#F5F5DC] via-[#F5F5DC] to-transparent h-30  flex rounded-md justify-center py-3">
-          <div className="flex flex-col items-center justify-end-safe">
-            <h1 className="poppins font-extrabold ">Macarrão</h1>
-            <p className="text-[10px] leading-2  text-center max-w-[150px] break-words">
-              Massa cozida temperada com sabor do nordeste
-            </p>
-          </div>
-        </div>
-      </div>
-    );
-  }
+  const {
+    isMenuCardapioOpen,
+    setisMenuCardapioOpen,
+    CardapioList,
+    FinalList,
+    setFinalList,
+    FilterListByCategory,
+    filterListByName,
+    itensPaginados,
+    paginaAtual,
+    setPaginaAtual,
+    totalPages,
+  } = useContext(ContextApp);
+
   return (
     <div>
       <div className="bg-[#F5F5DC] p-6 flex justify-center">
@@ -48,26 +41,65 @@ function CardapioContent() {
               } flex flex-col rounded-md bg-[#F5F5DC] p-6`}
             >
               <input
+                onChange={(e) => {
+                  filterListByName(e.target.value);
+                }}
                 className=" border-b-1 placeholder:text-gray-300 mb-4"
                 type="text"
                 placeholder="Buscar..."
               />
-              <button className="border-b-1 mb-4 text-left hover:text-amber-300">
+              <button
+                onClick={(e) => {
+                  FilterListByCategory(e.target.innerText);
+                  console.log(e.target.innerText);
+                }}
+                className="border-b-1 mb-4 text-left hover:text-amber-300"
+              >
                 Massas
               </button>
-              <button className="border-b-1 mb-4 text-left hover:text-amber-300">
+              <button
+                onClick={(e) => {
+                  FilterListByCategory(e.target.innerText);
+                  console.log(e.target.innerText);
+                }}
+                className="border-b-1 mb-4 text-left hover:text-amber-300"
+              >
                 Sanduiches
               </button>
-              <button className="border-b-1 mb-4 text-left hover:text-amber-300">
+              <button
+                onClick={(e) => {
+                  FilterListByCategory(e.target.innerText);
+                  console.log(e.target.innerText);
+                }}
+                className="border-b-1 mb-4 text-left hover:text-amber-300"
+              >
                 Salgados
               </button>
-              <button className="border-b-1 mb-4 text-left hover:text-amber-300">
+              <button
+                onClick={(e) => {
+                  FilterListByCategory(e.target.innerText);
+                  console.log(e.target.innerText);
+                }}
+                className="border-b-1 mb-4 text-left hover:text-amber-300"
+              >
                 Doces
               </button>
-              <button className="border-b-1 mb-4 text-left hover:text-amber-300">
+              <button
+                onClick={(e) => {
+                  FilterListByCategory(e.target.innerText);
+                  console.log(e.target.innerText);
+                }}
+                className="border-b-1 mb-4 text-left hover:text-amber-300"
+              >
                 Bebidas Quentes
               </button>
-              <button className="border-b-1 mb-4 text-left hover:text-amber-300">
+              <button
+                onClick={(e) => {
+                  FilterListByCategory(e.target.innerText);
+                  console.log(e.target.innerText);
+                }}
+                className="border-b-1 mb-4 text-left hover:text-amber-300"
+              >
                 Bebidas frias
               </button>
             </div>
@@ -75,45 +107,116 @@ function CardapioContent() {
 
           <div className="hidden lg:flex  gap-10 text-white p-6 justify-center">
             <input
+              onChange={(e) => {
+                filterListByName(e.target.value);
+              }}
               className=" border-b-1 placeholder:text-gray-300 mb-4"
               type="text"
               placeholder="Buscar..."
             />
-            <button className="border-b-1 mb-4 text-left hover:text-amber-300">
+            <button
+              onClick={(e) => {
+                FilterListByCategory(e.target.innerText);
+                console.log(e.target.innerText);
+              }}
+              className="border-b-1 mb-4 text-left hover:text-amber-300"
+            >
               Massas
             </button>
-            <button className="border-b-1 mb-4 text-left hover:text-amber-300">
+            <button
+              onClick={(e) => {
+                FilterListByCategory(e.target.innerText);
+                console.log(e.target.innerText);
+              }}
+              className="border-b-1 mb-4 text-left hover:text-amber-300"
+            >
               Sanduiches
             </button>
-            <button className="border-b-1 mb-4 text-left hover:text-amber-300">
+            <button
+              onClick={(e) => {
+                FilterListByCategory(e.target.innerText);
+                console.log(e.target.innerText);
+              }}
+              className="border-b-1 mb-4 text-left hover:text-amber-300"
+            >
               Salgados
             </button>
-            <button className="border-b-1 mb-4 text-left hover:text-amber-300">
+            <button
+              onClick={(e) => {
+                FilterListByCategory(e.target.innerText);
+                console.log(e.target.innerText);
+              }}
+              className="border-b-1 mb-4 text-left hover:text-amber-300"
+            >
               Doces
             </button>
-            <button className="border-b-1 mb-4 text-left hover:text-amber-300">
+            <button
+              onClick={(e) => {
+                FilterListByCategory(e.target.innerText);
+                console.log(e.target.innerText);
+              }}
+              className="border-b-1 mb-4 text-left hover:text-amber-300"
+            >
               Bebidas Quentes
             </button>
-            <button className="border-b-1 mb-4 text-left hover:text-amber-300">
+            <button
+              onClick={(e) => {
+                FilterListByCategory(e.target.innerText);
+                console.log(e.target.innerText);
+              }}
+              className="border-b-1 mb-4 text-left hover:text-amber-300"
+            >
               Bebidas frias
             </button>
           </div>
           <div className="flex flex-col items-center gap-8">
-            <div className="text-3xl flex  text-white poppins">
-              <h1>Title</h1>
-            </div>
             <div className="grid lg:grid-cols-4 gap-y-12 gap-6">
-              {tempdiv()}
-              {tempdiv()}
-              {tempdiv()}
-              {tempdiv()}
-              {tempdiv()}
-              {tempdiv()}
-              {tempdiv()}
-              {tempdiv()}
+              {itensPaginados.map((elements) => {
+                return (
+                  <div
+                    className={` rounded-md shadow-2xl shadow-black  bg-center bg-cover grid grid-rows-2 h-[190px] w-[210px] hover:scale-110`}
+                    style={{
+                      backgroundImage: `url(${elements.img})`,
+                    }}
+                  >
+                    <div></div>
+                    <div className="bg-gradient-to-t from-[#F5F5DC] via-[#F5F5DC] to-transparent h-30  flex rounded-md justify-center py-3">
+                      <div className="flex flex-col items-center justify-end-safe">
+                        <h1 className="poppins font-extrabold ">
+                          {elements.title}
+                        </h1>
+                        <p className="text-[10px] leading-2  text-center max-w-[150px] break-words">
+                          {elements.description}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
             </div>
           </div>
         </div>
+      </div>
+      <div className="bg-[#2E4F4F] flex justify-center gap-2">
+        <button
+          onClick={() => setPaginaAtual((prev) => Math.max(prev - 1, 1))}
+          className={`bg-[#F5F5DC] p-1 rounded-md text-[#2E4F4F] hover:bg-amber-300 poppins ${
+            paginaAtual === 1 ? "hidden" : ""
+          }`}
+        >
+          Anterior
+        </button>
+        <button
+          onClick={() => {
+            setPaginaAtual((prev) => prev + 1);
+            console.log(totalPages);
+          }}
+          className={`bg-[#F5F5DC] p-1 rounded-md text-[#2E4F4F] hover:bg-amber-300 poppins ${
+            paginaAtual === totalPages ? "hidden" : ""
+          }`}
+        >
+          Proximo
+        </button>
       </div>
     </div>
   );
